7ef84a6c5be030ece77b3a59da58c66f12cc866c
maisa abusalem <heart.webd@gmail.com>
2022-03-02 14:10:45 +0100

    arrange code

--- js/main.js
+++ js/main.js
@@ -32,87 +32,87 @@ previousBtn.addEventListener('click', selectPreviousBtn);
 
 async function fetchData(e) {
 
-  try {
-    let response = await fetch('https://api.thecatapi.com/v1/breeds')
+    try {
+        let response = await fetch('https://api.thecatapi.com/v1/breeds')
 
-    if (!response.ok) {
-      throw new Error('Something went wrong with the server');
-    }
-
-    let breeds = await response.json();
-
-
-    let optionItemsHTML = `<option>Select a breed</option>`;
-
-
-    for (let breed of breeds) {
-
-
-      optionItemsHTML += `<option id=${breed.id}>${breed.name}</option>`
-
-     
-    }
-
-
-    list.innerHTML = optionItemsHTML;
-  
-    list.addEventListener('change', function () {
-
-      var options = list.querySelectorAll("#list option");
-
-      for (let option of options) {
-        var selectedBreedId = option.id;
-        details.className = 'show';
-        if (list.value !== 'Select a breed') {
-          if (list.value === option.value) {
-
-            fetchDataByBreedId(selectedBreedId);
-  
-          }
+        if (!response.ok) {
+            throw new Error('Something went wrong with the server');
         }
-        else {
-          details.innerHTML = '';
-          details.className = 'hide';
+
+        let breeds = await response.json();
+
+
+        let optionItemsHTML = `<option>Select a breed</option>`;
+
+
+        for (let breed of breeds) {
+
+
+            optionItemsHTML += `<option id=${breed.id}>${breed.name}</option>`
+
+
         }
 
 
-      }
+        list.innerHTML = optionItemsHTML;
 
-    });
+        list.addEventListener('change', function () {
+
+            var options = list.querySelectorAll("#list option");
+
+            for (let option of options) {
+                var selectedBreedId = option.id;
+                details.className = 'show';
+                if (list.value !== 'Select a breed') {
+                    if (list.value === option.value) {
+
+                        fetchDataByBreedId(selectedBreedId);
+
+                    }
+                }
+                else {
+                    details.innerHTML = '';
+                    details.className = 'hide';
+                }
 
 
-  } catch (error) {
-    console.log(error);
-  }
+            }
+
+        });
+
+
+    } catch (error) {
+        console.log(error);
+    }
 }
 //fetch data by page and limit order by Desc(how many images will be shown per page)
 async function fetchAllData(selectedLimit, number) {
 
     try {
-      let URL = `https://api.thecatapi.com/v1/images/search?limit=${selectedLimit}&page=${number}&order=DESC`;
-      fetchPaginatingUrl(URL);
-  
+        let URL = `https://api.thecatapi.com/v1/images/search?limit=${selectedLimit}&page=${number}&order=DESC`;
+        fetchPaginatingUrl(URL);
+
     } catch (error) {
-      console.log(error);
+        console.log(error);
     }
-  }
-  //call url
-  async function fetchPaginatingUrl(URL){
+}
+//call url
+async function fetchPaginatingUrl(URL) {
     let response = await fetch(URL, {
         method: "GET",
         headers: {
-          'x-api-key': '93c56b63-d3ca-4ebd-905e-7a0df717877f'
+            'x-api-key': '93c56b63-d3ca-4ebd-905e-7a0df717877f'
         }
-      })
-    
-      if (!response.ok) {
+    })
+
+    if (!response.ok) {
         throw new Error('Something went wrong...');
-      }
-      let catsImageUrl = ''
-      let pages = await response.json();
-  
-      for (let page of pages) {
-  
+    }
+    let catsImageUrl = ''
+    let pages = await response.json();
+
+    for (let page of pages) {
+
         catsImageUrl = page.url;
         catImage.innerHTML += `
     
@@ -124,30 +124,30 @@ async function fetchAllData(selectedLimit, number) {
         </div>
         
         `
-  pageNumber.innerHTML = `Page: ${number}`;
-      }
-  }
-  //fetch data by breed's id
-  async function fetchDataByBreedId(selectedBreedId) {
-  
-   
+        pageNumber.innerHTML = `Page: ${number}`;
+    }
+}
+//fetch data by breed's id
+async function fetchDataByBreedId(selectedBreedId) {
+
+
     try {
-      let response = await fetch('https://api.thecatapi.com/v1/breeds/' + selectedBreedId, {
-        method: "GET",
-        withCredentials: true,
-        headers: {
-          'X-Api-Key': '93c56b63-d3ca-4ebd-905e-7a0df717877f'
+        let response = await fetch('https://api.thecatapi.com/v1/breeds/' + selectedBreedId, {
+            method: "GET",
+            withCredentials: true,
+            headers: {
+                'X-Api-Key': '93c56b63-d3ca-4ebd-905e-7a0df717877f'
+            }
+        })
+
+        if (!response.ok) {
+            throw new Error('Something went wrong with the server');
         }
-      })
-  
-      if (!response.ok) {
-        throw new Error('Something went wrong with the server');
-      }
-  
-      let breeds = await response.json();
-  
-      let HTMLContent = ''
-      HTMLContent += `
+
+        let breeds = await response.json();
+
+        let HTMLContent = ''
+        HTMLContent += `
   
       <a href="#" class="d-block mb-4 h-100" ><h3 id=${breeds.id}>${breeds.name}</h3></a>
         <i><b>Origin: </b>${breeds.origin}</i>
@@ -156,150 +156,150 @@ async function fetchAllData(selectedLimit, number) {
   
   
   `
-  
-  
-      details.innerHTML = HTMLContent;
-      let title = document.querySelector('#details a h3 ');
-  
-      title.addEventListener('click', function (e) {
-        e.preventDefault();
-        let theClickedPostLink = e.target;
-        selectedBreedId = theClickedPostLink.id;
-        // console.log(selectedBreedId);
-        let moreContent = theClickedPostLink.parentNode.parentNode.parentNode.nextElementSibling;
-        // console.log(moreContent);
-        fetchDataByBreedId(selectedBreedId);
-  
-  
-        moreContent.innerHTML = `
+
+
+        details.innerHTML = HTMLContent;
+        let title = document.querySelector('#details a h3 ');
+
+        title.addEventListener('click', function (e) {
+            e.preventDefault();
+            let theClickedPostLink = e.target;
+            selectedBreedId = theClickedPostLink.id;
+            // console.log(selectedBreedId);
+            let moreContent = theClickedPostLink.parentNode.parentNode.parentNode.nextElementSibling;
+            // console.log(moreContent);
+            fetchDataByBreedId(selectedBreedId);
+
+
+            moreContent.innerHTML = `
           <p><b>Description:</b> ${breeds.description}</p>
           <p id="rating">${getAdaptability(breeds.adaptability)}</p>
           <p id="rating">${getGrooming(breeds.grooming)}</p>
           <p id="rating">${getIndoor(breeds.indoor)}</p>
          
        `
-        moreContent.classList.toggle('show');
-      })
+            moreContent.classList.toggle('show');
+        })
     }
-  
-  
+
+
     catch (error) {
-      console.log(error);
+        console.log(error);
     }
-  }
-  //shows number of images per page
-  function selectLimitOptions(e){
+}
+//shows number of images per page
+function selectLimitOptions(e) {
     e.preventDefault();
     catImage.innerHTML = ''
     selectedLimit = limitList.value;
     fetchAllData(selectedLimit, number);
-  }
-  //shows previous page
-  function selectPreviousBtn(e){
+}
+//shows previous page
+function selectPreviousBtn(e) {
     e.preventDefault();
     catImage.innerHTML = ''
     number = number - 1;
     if (number < 0) number = 10;
     fetchAllData(selectedLimit, number);
-  }
-  //show next page
-  function selectNextBtn(e){
+}
+//show next page
+function selectNextBtn(e) {
     e.preventDefault();
     catImage.innerHTML = ''
     number = number + 1;
     if (number > 10) number = 0
     fetchAllData(selectedLimit, number);
-  }
-  //converts adaptability value (1-5) to star rating
-  function getAdaptability(adaptability) {
+}
+//converts adaptability value (1-5) to star rating
+function getAdaptability(adaptability) {
     switch (adaptability) {
-      case 0:
-  
-        return `Adaptability<br><span class="rating-static rating-10"></span>`;
-        break;
-      case 1:
-        return `Adaptability<br><span class="rating-static rating-10"></span>`;
-  
-        break;
-      case 2:
-        return `Adaptability:<span class="rating-static rating-20"></span>`;
-  
-        break;
-      case 3:
-        return `Adaptability:<span class="rating-static rating-30"></span>`;
-  
-        break;
-      case 4:
-        return `Adaptability:<span class="rating-static rating-40"></span>`;
-  
-        break;
-      case 5:
-        return `Adaptability: <span class="rating-static rating-50"></span>`;
-  
-        break;
-      default:
-      // code block
+        case 0:
+
+            return `Adaptability<br><span class="rating-static rating-10"></span>`;
+            break;
+        case 1:
+            return `Adaptability<br><span class="rating-static rating-10"></span>`;
+
+            break;
+        case 2:
+            return `Adaptability:<span class="rating-static rating-20"></span>`;
+
+            break;
+        case 3:
+            return `Adaptability:<span class="rating-static rating-30"></span>`;
+
+            break;
+        case 4:
+            return `Adaptability:<span class="rating-static rating-40"></span>`;
+
+            break;
+        case 5:
+            return `Adaptability: <span class="rating-static rating-50"></span>`;
+
+            break;
+        default:
+        // code block
     }
-  
-  
-  }
-  //converts grooming value (1-5) to star rating
-  function getGrooming(grooming) {
+
+
+}
+//converts grooming value (1-5) to star rating
+function getGrooming(grooming) {
     switch (grooming) {
-      case 0:
-  
-        return `Grooming: <span class="rating-static rating-0"></span>`;
-        break;
-      case 1:
-  
-        return `Grooming: <span class="rating-static rating-10"></span>`;
-        break;
-      case 2:
-  
-        return `Grooming: <span class="rating-static rating-20"></span>`;
-        break;
-      case 3:
-  
-        return `Grooming: <span class="rating-static rating-30"></span>`;
-        break;
-      case 4:
-  
-        return `Grooming: <span class="rating-static rating-40"></span>`;
-        break;
-      case 5:
-        return `Grooming: <span class="rating-static rating-50"></span>`;
-        break;
-      default:
-      // code block
+        case 0:
+
+            return `Grooming: <span class="rating-static rating-0"></span>`;
+            break;
+        case 1:
+
+            return `Grooming: <span class="rating-static rating-10"></span>`;
+            break;
+        case 2:
+
+            return `Grooming: <span class="rating-static rating-20"></span>`;
+            break;
+        case 3:
+
+            return `Grooming: <span class="rating-static rating-30"></span>`;
+            break;
+        case 4:
+
+            return `Grooming: <span class="rating-static rating-40"></span>`;
+            break;
+        case 5:
+            return `Grooming: <span class="rating-static rating-50"></span>`;
+            break;
+        default:
+        // code block
     }
-  }
-  //converts indoor value (1-5) to star rating
-  function getIndoor(indoor) {
+}
+//converts indoor value (1-5) to star rating
+function getIndoor(indoor) {
     switch (indoor) {
-      case 0:
-  
-        return `Indoor: <span class="rating-static rating-0"></span>`;
-        break;
-      case 1:
-  
-        return `Indoor: <span class="rating-static rating-10"></span>`;
-        break;
-      case 2:
-  
-        return `Indoor: <span class="rating-static rating-20"></span>`;
-        break;
-      case 3:
-  
-        return `Indoor: <span class="rating-static rating-30"></span>`;
-        break;
-      case 4:
-  
-        return `Indoor: <span class="rating-static rating-40"></span>`;
-        break;
-      case 5:
-        return `Indoor: <span class="rating-static rating-50"></span>`;
-        break;
-      default:
-      // code block
+        case 0:
+
+            return `Indoor: <span class="rating-static rating-0"></span>`;
+            break;
+        case 1:
+
+            return `Indoor: <span class="rating-static rating-10"></span>`;
+            break;
+        case 2:
+
+            return `Indoor: <span class="rating-static rating-20"></span>`;
+            break;
+        case 3:
+
+            return `Indoor: <span class="rating-static rating-30"></span>`;
+            break;
+        case 4:
+
+            return `Indoor: <span class="rating-static rating-40"></span>`;
+            break;
+        case 5:
+            return `Indoor: <span class="rating-static rating-50"></span>`;
+            break;
+        default:
+        // code block
     }
-  }
+}
